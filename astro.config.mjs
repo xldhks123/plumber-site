6f4fc7c919b716253b639b3c3301aa9402bb89fe
xldhks123 <xldhks140@gmail.com>
2026-06-07 23:11:04 +0900

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://eclectic-muffin-1e6f44.netlify.app',
+  integrations: [sitemap()]
+});
